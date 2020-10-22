627d4279b1c897e140a588273728e66d6564b3b9
Jie Wang <jie.w@wustl.edu>
2020-10-21 21:27:14 -0500

    modify bash file to include four python programs

--- startup.sh
+++ startup.sh
@@ -18,6 +18,23 @@ done
 sudo sysctl -w net.core.rmem_max=24862979
 sudo sysctl -w net.core.wmem_max=24862979
 
+sudo rm /usr/bin/uhd_rx_cfile
+chmod +x /local/repository/uhd_rx_cfile
+sudo mv /local/repository/uhd_rx_cfile /usr/bin/
+
+chmod +x /local/repository/uhd_siggen_timing
+sudo mv /local/repository/uhd_siggen_timing /usr/bin/
+
+chmod +x /local/repository/uhd_siggen_45sec
+sudo mv /local/repository/uhd_siggen_45sec /usr/bin/
+
+chmod +x /local/repository/uhd_siggen_90sec
+sudo mv /local/repository/uhd_siggen_90sec /usr/bin/
+
+chmod +x /local/repository/iq_to_power.py
+sudo mv /local/repository/iq_to_power.py /usr/bin/
+
+
 sudo ed /etc/sysctl.conf << "EDEND"
 a
 net.core.rmem_max=24862979
